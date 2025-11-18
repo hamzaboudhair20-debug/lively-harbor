c0a9f9a7207fe555ff5c18d3b7d6ad8a7e2373fe
HAMZA BOUDHAIR <hamza.boudhair.20@edu.uiz.ac.ma>
2025-11-18 19:48:28 +0100

    Update index.m

--- content/education/master/index.m
+++ content/education/master/index.m
@@ -1,1 +1,17 @@
+---
+title: "Master of Science in Electronics and Advanced Materials for Renewable Energies (EMAENR)"
+organization: "Polydisciplinary Faculty of Ouarzazate – Ibn Zohr University"
+location: "Ouarzazate, Morocco"
+date_start: 2023-09-01
+date_end: 2025-06-30
+summary: |
+  A graduate program focused on electronics, advanced materials, photovoltaic systems,
+  wind energy conversion, hydrogen technologies, thermal energy storage, and intelligent
+  energy systems using AI and Python.  
+  Research topic: Aerodynamic modelling and performance optimization of a HAWT using
+  QBlade (BEMT, aero-structural simulation, and performance curves).
+image:
+  filename: "master-logo.png"
+  focal_point: "center"
+---
 
